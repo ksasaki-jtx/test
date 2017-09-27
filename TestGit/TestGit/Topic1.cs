48d6729b2d817b55fe5ad774e10f0b2b6f35af48
ksasaki-jtx <kosuke.sasaki@japantaxi.co.jp>
2017-09-27 10:35:01 +0900

    topic1_master

--- TestGit/TestGit/Topic1.cs
+++ TestGit/TestGit/Topic1.cs
@@ -8,5 +8,17 @@ namespace TestGit
 {
     class Topic1
     {
+        public int Hoge1 { get; set; }
+
+        public int Hoge01 { get; set; }
+
+        public int Hoge2 { get; set; }
+
+        public int Hoge02 { get; set; }
+
+        public int Hoge3 { get; set; }
+
+        public int Hoge03 { get; set; }
     }
+
 }
